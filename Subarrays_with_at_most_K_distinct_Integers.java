9275c8195f17e6c22a39ec673363eebf055460e3
Aniket Raj <aniketraj0224@gmail.com>
2026-01-09 00:12:41 +0530

    Create Subarrays_with_at_most_K_distinct_Integers.java

---
+++ Subarrays_with_at_most_K_distinct_Integers.java
@@ -0,0 +1,28 @@
+// PROBLEM : Subarrays With At Most K Distinct Integers
+
+// SOLUTION :
+
+class Solution {
+    public int countAtMostK(int arr[], int k) {
+        // code here
+        int n = arr.length;
+        int left = 0;
+        int count = 0;
+        
+        Map<Integer, Integer> freq = new HashMap<>();
+        
+        for(int right = 0; right < n; right++){
+            freq.put(arr[right], freq.getOrDefault(arr[right], 0)+1);
+            
+            while(freq.size() > k){
+                freq.put(arr[left], freq.get(arr[left]) - 1);
+                if(freq.get(arr[left]) == 0){
+                    freq.remove(arr[left]);
+                }
+                left++;
+            }
+            count += (right - left + 1);
+        }
+        return count;
+    }
+}
