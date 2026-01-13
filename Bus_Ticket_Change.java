9fb70447988430ae849e7e64b980d092fe3d6664
Aniket Raj <aniketraj0224@gmail.com>
2026-01-13 20:58:05 +0530

    Create Bus_Ticket_Change.java

---
+++ Bus_Ticket_Change.java
@@ -0,0 +1,33 @@
+// PROBLEM : Bus Ticket Change
+
+// SOLUTION :
+
+class Solution {
+    public boolean canServe(int[] arr) {
+        // code here
+        int five = 0;
+        int ten =0;
+        
+        for(int bill:arr){
+            if(bill == 5){
+                five++;
+            }
+            else if(bill == 10){
+                if(five == 0) return false;
+                five--;
+                ten++;
+            }
+            else{
+                if(ten > 0 && five > 0){
+                    ten--;
+                    five--;
+                } else if(five >= 3){
+                    five -= 3;
+                }else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
