60500e4397419b74e6aca2b79c3f3f284c5af4cb
Aniket Raj <aniketraj0224@gmail.com>
2026-01-15 00:53:22 +0530

    Create Candy.java

---
+++ Candy.java
@@ -0,0 +1,34 @@
+// PROBLEM : Candy
+
+// SOLUTION :
+
+class Solution {
+    public int minCandy(int arr[]) {
+        // code here
+        int n = arr.length;
+        if(n == 0) return 0;
+        
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+        
+        for(int i= 1; i<n ; i++){
+            if(arr[i] > arr[i-1]){
+                candies[i] = candies[i-1] + 1;
+            }
+        }
+        
+        for(int i = n-2; i >= 0; i--){
+            if(arr[i] > arr[i+1]){
+                candies[i] = Math.max(candies[i], candies[i+1] + 1);
+            }
+        }
+        
+        int total = 0;
+        for(int c : candies){
+            total += c;
+        }
+        
+        return total;
+        
+    }
+}
