fb14b2087d56cc76336659267eb3ff429ff00ada
Aniket Raj <aniketraj0224@gmail.com>
2026-03-04 19:56:27 +0530

    Create Max Xor Subarray of size K.java

---
+++ Max Xor Subarray of size K.java
@@ -0,0 +1,29 @@
+// PROBLEM : Max Xor Subarray of size K
+
+// SOLUTION :
+
+class Solution {
+    public int maxSubarrayXOR(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        if(n < k) return 0;
+        
+        int currentXor =0;
+        
+        for(int i=0; i<k; i++){
+            currentXor ^= arr[i];
+        }
+        
+        int maxXor = currentXor;
+        
+        for(int i=k; i<n;i++){
+            currentXor = currentXor ^ arr[i-k]^arr[i];
+            
+            if(currentXor > maxXor){
+                maxXor = currentXor;
+                
+            }
+        }
+        return maxXor;
+    }
+}
