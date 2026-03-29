1e41a9f4dac93fdb2c32881b8fa54842f28abe61
Aniket Raj <aniketraj0224@gmail.com>
2026-03-29 09:35:50 +0530

    Create Partitions with Given Difference.java

---
+++ Partitions with Given Difference.java
@@ -0,0 +1,28 @@
+// PROBLEM : Partitions with Given Difference
+
+// SOLUTION :
+
+class Solution {
+    public int countPartitions(int[] arr, int diff) {
+        int totalSum = 0;
+        for (int num : arr) {
+            totalSum += num;
+        }
+
+        // Check feasibility
+        if ((totalSum + diff) % 2 != 0) return 0;
+
+        int target = (totalSum + diff) / 2;
+
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+
+        for (int num : arr) {
+            for (int j = target; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+
+        return dp[target];
+    }
+}
