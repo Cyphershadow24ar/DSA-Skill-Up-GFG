750689751a6bcb19492c1a4043e464491b7be562
Aniket Raj <aniketraj0224@gmail.com>
2026-04-19 10:38:55 +0530

    Create Check_for_power.java

---
+++ Check_for_power.java
@@ -0,0 +1,19 @@
+// PROBLEM : Check for Power
+
+// SOLUTION :
+
+class Solution {
+    boolean isPower(int x, int y) {
+        
+        // Special case
+        if (x == 1) return y == 1;
+        
+        // Keep dividing y by x
+        while (y % x == 0) {
+            y = y / x;
+        }
+        
+        // If we reach 1, it's a power
+        return y == 1;
+    }
+}
