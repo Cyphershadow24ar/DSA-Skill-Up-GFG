100e425520fbd9cb35df741180125f44afce0028
Aniket Raj <aniketraj0224@gmail.com>
2026-04-30 10:24:26 +0530

    Create Check if an Array is Max Heap.java

---
+++ Check if an Array is Max Heap.java
@@ -0,0 +1,20 @@
+// PROBLEM : Check if an Array is Max Heap
+
+// SOLUTION :
+
+class Solution {
+    public boolean isMaxHeap(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i <= (n / 2) - 1; i++) {
+            
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+
+            if (l < n && arr[i] < arr[l]) return false;
+            if (r < n && arr[i] < arr[r]) return false;
+        }
+
+        return true;
+    }
+}
