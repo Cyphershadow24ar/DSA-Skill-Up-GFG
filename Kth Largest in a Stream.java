4ed1327edcd262ffb937e750bd784abc1036d7b1
Aniket Raj <aniketraj0224@gmail.com>
2026-05-01 00:11:33 +0530

    Create Kth Largest in a Stream.java

---
+++ Kth Largest in a Stream.java
@@ -0,0 +1,28 @@
+// PROBLEM : Kth Largest in a Stream
+
+// SOLUTION :
+
+import java.util.*;
+
+class Solution {
+    static ArrayList<Integer> kthLargest(int[] arr, int k) {
+        ArrayList<Integer> res = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            pq.add(arr[i]);
+            
+            if (pq.size() > k) {
+                pq.poll();
+            }
+            
+            if (pq.size() < k) {
+                res.add(-1);
+            } else {
+                res.add(pq.peek());
+            }
+        }
+        
+        return res;
+    }
+}
