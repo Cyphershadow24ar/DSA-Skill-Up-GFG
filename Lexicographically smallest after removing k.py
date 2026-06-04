3e3cbaf003409ba7498055129e2747b182c9316a
Aniket Raj <aniketraj0224@gmail.com>
2026-06-05 01:36:26 +0530

    Add lexicographicallySmallest function
    Implement function to find lexicographically smallest string after removing k characters.

---
+++ Lexicographically smallest after removing k.py
@@ -0,0 +1,36 @@
+# PROBLEM :
+
+# SOLTION :
+
+class Solution:
+    def lexicographicallySmallest(self, s, k):
+        # code here 
+        n = len(s)
+        
+        if n & (n-1) == 0:
+            k //= 2
+        else:
+            k *= 2
+            
+        
+        if k >= n:
+            return "-1"
+            
+        m = n-k
+        st = []
+        rem = k
+        
+        for c in s :
+            while st and rem and st[-1] > c:
+                st.pop()
+                rem -= 1
+            st.append(c)
+        
+        while rem:
+            st.pop()
+            rem -= 1
+            
+        
+        ans = ''.join(st[:m])
+        
+        return ans if ans else "-1"
