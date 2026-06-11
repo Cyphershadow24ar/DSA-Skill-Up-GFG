2a97272ff18eaa467848d37d20199fcd3d886f17
Aniket Raj <aniketraj0224@gmail.com>
2026-06-11 22:40:59 +0530

    Create Equal Point in Brackets.py

---
+++ Equal Point in Brackets.py
@@ -0,0 +1,23 @@
+# PROBLEM : Equal Point in Brackets
+
+# SOLUTION :
+
+class Solution:
+    def findIndex(self, s):
+        # code here 
+        closeRight = s.count(')')
+        openLeft = 0
+
+        for k in range(len(s) + 1):
+            if openLeft == closeRight:
+                return k
+
+            if k < len(s):
+                if s[k] == '(':
+                    openLeft += 1
+                else:
+                    closeRight -= 1
+
+        return len(s)
+
+
