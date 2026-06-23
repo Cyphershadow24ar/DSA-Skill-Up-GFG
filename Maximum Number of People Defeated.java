50e448909bbb86331c6fc373d8a2ca79be19bee9
Aniket Raj <aniketraj0224@gmail.com>
2026-06-23 22:17:37 +0530

    Implement solution for maximum number of people defeated

---
+++ Maximum Number of People Defeated.java
@@ -0,0 +1,15 @@
+ // PROBLEM : Maximum Number of People Defeated
+
+// SOLUTION :
+
+class Solution {
+    int maxPeopleDefeated(int p) {
+        // code here
+         int count = 0;
+        for(int i = 1; p >= i*i; i++){
+             p = p - (i*i);
+             count++;
+        }
+        return count;
+    }
+};
