776df7f7f213bccad3378a18db2ba507779484d9
Aniket Raj <aniketraj0224@gmail.com>
2026-08-20 20:48:51 +0530

    Create Node and Ancestor Max Diff.java

---
+++ Node and Ancestor Max Diff.java
@@ -0,0 +1,38 @@
+// PROBLEM : Node and Ancestor Max Diff
+
+// SOLUTION :
+
+/* Structure of binary tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    int res; 
+    
+    int maxDiff(Node root) {
+        //  code here
+        res = Integer.MIN_VALUE;
+        dfs(root);
+        return res;
+    }
+    
+    int dfs(Node node){
+        if(node == null) return Integer.MAX_VALUE;
+        
+        int l = dfs(node.left);
+        int r = dfs(node.right);
+        
+        res = Math.max(res, node.data - Math.min(l , r));
+        return Math.min(node.data, Math.min(l ,r));
+    }
+}
